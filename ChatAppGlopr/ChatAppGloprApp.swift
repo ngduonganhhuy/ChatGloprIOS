f3177a61c6bcad17dcfebde11c6dc55e2f1b586e
Nguyễn Dương Anh Huy <huynda@runsystem.net>
2023-10-22 12:43:57 +0700

    Initial Commit

---
+++ ChatAppGlopr/ChatAppGloprApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppGloprApp.swift
+//  ChatAppGlopr
+//
+//  Created by Huy Nguyễn Dương Anh on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppGloprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAppGlopr/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatAppGlopr
+//
+//  Created by Huy Nguyễn Dương Anh on 22/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
